c1f7d65bc71ea7df661483d5973004db7e2b9d62
Jean Ventura <jv@venturasystems.net>
2019-10-08 18:17:10 -0700

    Add compare_models utility function

--- pata/models/tests/test_utils.py
+++ pata/models/tests/test_utils.py
@@ -1,7 +1,14 @@
 """ Test for pata.models module. """
 import unittest
+
+from mock import (
+    MagicMock,
+    Mock,
+    )
+
 from pata.models.utils import (
     AuditMixin,
+    compare_models,
     )
 
 
@@ -18,3 +25,64 @@ class AuditMixinTests(unittest.TestCase):
         self.assertTrue(hasattr(mixin_obj, "created_at"))
         self.assertTrue(hasattr(mixin_obj, "modified_by"))
         self.assertTrue(hasattr(mixin_obj, "modified_at"))
+
+
+class CompareModelsTests(unittest.TestCase):
+    """ Tests for pata.models.utils.compare_models. """
+
+    def test_diff_same(self):
+        """ Test no difference with another object. """
+        # Given
+        column1 = Mock()
+        column1.configure_mock(name="field1")
+
+        model = "model1"
+        base_obj = MagicMock(
+            field1="same",
+            metadata=MagicMock(
+                tables={model: MagicMock(columns=[column1])}
+            ))
+        target_obj = MagicMock(field1="same")
+        expected_result = {}
+
+        # When
+        result = compare_models(model, base_obj, target_obj)
+
+        # Then
+        self.assertEqual(result, expected_result)
+
+    def test_diff(self):
+        """ Test difference with another object. """
+        column1 = Mock()
+        column1.configure_mock(name="id")
+        column2 = Mock()
+        column2.configure_mock(name="field1")
+        column3 = Mock()
+        column3.configure_mock(name="field2")
+        column4 = Mock()
+        column4.configure_mock(name="field3")
+
+        model = "model1"
+        base_obj = MagicMock(
+            id=9, field1="old value", field2=0, field3="same",
+            metadata=MagicMock(
+                tables={
+                    model: MagicMock(
+                        columns=[column1, column2, column3, column4])}
+            ))
+        target_obj = MagicMock(
+            field1="new value", field2=1, field3="same")
+        expected_result = {}
+        expected_result = {
+            "field1": {
+                "old": "old value", "new": "new value"},
+            "field2": {
+                "old": 0, "new": 1},
+            }
+
+        # When
+        result = compare_models(
+            model, base_obj, target_obj, exclude=("id",))
+
+        # Then
+        self.assertEqual(result, expected_result)
--- pata/models/utils.py
+++ pata/models/utils.py
@@ -1,4 +1,11 @@
 """ Utility models """
+from typing import (
+    Any,
+    Dict,
+    Tuple,
+    Union,
+    )
+
 from sqlalchemy import (
     Column,
     String,
@@ -12,3 +19,51 @@ class AuditMixin():  # pylint: disable=too-few-public-methods
     created_at = Column(TIMESTAMP(timezone=True))
     modified_by = Column(String(64))
     modified_at = Column(TIMESTAMP(timezone=True))
+
+
+def compare_models(
+        model: str, base: Any, target: Any, exclude: Tuple[str, ...] = ()
+        ) -> Dict[str, Dict[str, Union[str, int]]]:
+    """
+    Compare two models.
+
+    Expected type for both objects is the same sqlalchemy.orm.model.
+
+    Parameters
+    ----------
+    model : str
+        Name of model for objects.
+    base : sqlalchemy.orm.model
+        Object to copmare.
+    target : sqlalchemy.orm.model
+        Object to compare to.
+    exclude : tuple
+        List of fields to exclude.
+
+    Returns
+    -------
+    dict
+
+    Example
+    -------
+    output:
+        {
+            "field_name": {"old": 0, "new": 1},
+            ...
+        }
+
+    """
+    result = {}
+    column_names = [
+        column.name
+        for column in base.metadata.tables[model].columns
+        if column.name not in exclude
+        ]
+
+    for column_name in column_names:
+        old_value = getattr(base, column_name)
+        new_value = getattr(target, column_name)
+        if old_value != new_value:
+            result[column_name] = {"old": old_value, "new": new_value}
+
+    return result
